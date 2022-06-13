769a6b67fc3b11c9f6de6b83740358be941c7f5d
David(gecert) <dkarandze4@gmail.com>
2022-06-13 11:26:02 +0200

    remember to add .idea

--- parse.py
+++ parse.py
@@ -1,16 +1,30 @@
 from urllib.parse import urlsplit, parse_qs
+
+
 def parse(query: str) -> dict:
     url = query
     query = urlsplit(url).query
     params = parse_qs(query)
     return {k: v[0] for k, v in params.items()}
 
+
 if __name__ == '__main__':
     print('Example:')
-    print(parse('https://example.com/path/to/page?name=ferret&color=blue'))
+    print(parse('http://example.com/?name=D+A+V+I+D'))
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
     assert parse('https://example.com/path/to/page?name=ferret&color=purple&') == {'name': 'ferret', 'color': 'purple'}
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+
+    assert parse('http://example.com/?name=David&sex=male&') == {'name': 'David', 'sex': 'male'}
+    assert parse('http://example.com/?name=DaviD') == {'name': 'DaviD'}
+    assert parse('http://example.com?name=DAVID&pers=QueenOfPain&lvl=30') == {'name': 'DAVID', 'pers': 'QueenOfPain', 'lvl': '30'}
+    assert parse('http://example.com/?names=david/maria') == {'names': 'david/maria'}
+    assert parse('') == {}
+    assert parse('http://example.com/???name=Dima&&&&&&&&pers=123123#name=david') == {'name': 'Dima', 'pers': '123123'}
+    assert parse('http://example.com/?pers=Invoker&atribut=Intelligence') == {'pers': 'Invoker', 'atribut': 'Intelligence'}
+    assert parse('http://example.com/?$$$$$&&&&&&#####') == {}
+    assert parse('http://example.com/?name=Dima$#$#$#$#$#$#$') == {'name': 'Dima$'}
+    assert parse('http://example.com/?name=D+A+V+I+D') == {'name': 'D A V I D'}
     print('Wow, you are doing pretty good. Time to check it!')
--- parse_cookie.py
+++ parse_cookie.py
@@ -1,4 +1,6 @@
 from urllib.parse import urlsplit, parse_qs
+
+
 def parse_cookie(query: str) -> dict:
     url = query
     query = urlsplit(url).path
@@ -9,21 +11,20 @@ def parse_cookie(query: str) -> dict:
 
 if __name__ == '__main__':
     print('Example:')
-    print(parse_cookie('name=Dima;age=28;'))
+    print(parse_cookie('name=Dima#http://example.com/?$$$$$&&&&&&#####'))
     assert parse_cookie('name=Dima;') == {'name': 'Dima'}
     assert parse_cookie('') == {}
     assert parse_cookie('name=Dima;age=28;') == {'name': 'Dima', 'age': '28'}
     assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
 
-    assert parse_cookie('name=Axe;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Maria;age=21;') == {'name': 'Maria', 'age': '21'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('"''"') == {}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-    assert parse_cookie('name=Dima=User;age=28;') == {'name': 'Dima=User', 'age': '28'}
-
+    assert parse_cookie('name=David&sex=male&') == {'name': 'David', 'sex': 'male'}
+    assert parse_cookie('name=DaviD') == {'name': 'DaviD'}
+    assert parse_cookie('name=DAVID;pers=QueenOfPain&lvl=30') == {'name': 'DAVID', 'pers': 'QueenOfPain', 'lvl': '30'}
+    assert parse_cookie('names=david/maria') == {'names': 'david/maria'}
+    assert parse_cookie('') == {}
+    assert parse_cookie('age=21;name=Dima&;pers=123123#name=david#') == {'age': '21', 'name': 'Dima', 'pers': '123123'}
+    assert parse_cookie('pers=Invoker&atribut=Intelligence') == {'pers': 'Invoker', 'atribut': 'Intelligence'}
+    assert parse_cookie('http://example.com/?$$$$$&&&&&&#####') == {}
+    assert parse_cookie('name=Dima#http://example.com/?$$$$$&&&&&&#####') == {'name': 'Dima'}
+    assert parse_cookie('name=D+A+V+I+D') == {'name': 'D A V I D'}
     print('Wow, you are doing pretty good. Time to check it!')
